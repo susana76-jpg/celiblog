391deff9143c69179273a3568d14403cbc7789c6
susana76-jpg <susanaricovara@gmail.com>
2025-11-14 19:51:12 +0100

    Allowed Cors
    Allowed Cors

--- celiblog-api/src/main/java/com/daw/celiblog/config/WebConfig.java
+++ celiblog-api/src/main/java/com/daw/celiblog/config/WebConfig.java
@@ -7,9 +7,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
+        registry.addMapping("/api/**")
                 .allowedOrigins("http://localhost:8081")
-                .allowedMethods("GET", "POST", "PUT", "DELETE");
+                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://46.183.113.124:8081")
+                .allowedOrigins("http://46.183.113.124:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true);
     }
 
 }
