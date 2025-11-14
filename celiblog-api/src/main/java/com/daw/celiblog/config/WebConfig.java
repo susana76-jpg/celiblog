5093316245fba83dc021ab1df3417d6c70fb9097
susana76-jpg <susanaricovara@gmail.com>
2025-11-14 22:34:42 +0100

    Permitir todas las conexiones
    Permitir todas las conexiones

--- celiblog-api/src/main/java/com/daw/celiblog/config/WebConfig.java
+++ celiblog-api/src/main/java/com/daw/celiblog/config/WebConfig.java
@@ -7,13 +7,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:8081")
-                .allowedOrigins("http://localhost:3000")
-                .allowedOrigins("http://46.183.113.124:8081")
-                .allowedOrigins("http://46.183.113.124:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowCredentials(true);
+        registry.addMapping("/**")
+                .allowedOrigins("*")   // Permitir todos los orígenes
+                .allowedMethods("*")   // Permitir todos los métodos (GET, POST, PUT, DELETE, etc.)
+                .allowedHeaders("*");  // Permitir todos los headers
     }
 
 }
