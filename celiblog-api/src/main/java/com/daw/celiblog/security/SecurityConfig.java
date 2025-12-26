ac4e9115b058d153e2909e640d6e367d3704771f
susana76-jpg <susanaricovara@gmail.com>
2025-12-26 18:34:36 +0100

    api nginx

--- celiblog-api/src/main/java/com/daw/celiblog/security/SecurityConfig.java
+++ celiblog-api/src/main/java/com/daw/celiblog/security/SecurityConfig.java
@@ -64,7 +64,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost", "http://localhost:3000", "http://46.183.113.124", "http://46.183.113.124:8081", "http://46.183.113.124:3000", "http://celiblog.es", "http://www.celiblog.es"));
+        configuration.setAllowedOrigins(List.of("http://localhost", "http://localhost:3000", "http://46.183.113.124", "http://46.183.113.124:8081", "http://46.183.113.124:3000", "http://celiblog.es", "http://www.celiblog.es", "https://api.celiblog.es"));
         configuration.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         configuration.setAllowedHeaders(List.of("Content-Type","Authorization"));
         configuration.setAllowCredentials(true);
