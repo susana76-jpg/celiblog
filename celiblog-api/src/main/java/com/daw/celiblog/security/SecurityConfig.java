ff5b6c4eae53fd0e581b95ac1de0ec8de722e782
susana76-jpg <susanaricovara@gmail.com>
2025-12-27 10:13:14 +0100

    Update SecurityConfig.java

--- celiblog-api/src/main/java/com/daw/celiblog/security/SecurityConfig.java
+++ celiblog-api/src/main/java/com/daw/celiblog/security/SecurityConfig.java
@@ -68,7 +68,7 @@ public class SecurityConfig {
         configuration.setAllowedOrigins(List.of("*"));
         configuration.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         configuration.setAllowedHeaders(List.of("Content-Type","Authorization"));
-        configuration.setAllowCredentials(true);
+        configuration.setAllowCredentials(false);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
